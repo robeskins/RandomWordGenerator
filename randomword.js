425a6dfcb2a6dee83b7d910e11e46ec11cda2b64
robeskins <rjeskins@gmail.com>
2023-01-06 15:03:51 +0000

    Second Commit, added variables

--- randomword.js
+++ randomword.js
@@ -3,18 +3,18 @@ function createMessage(A, B, C){
 };
 
 function location(){
-    location = ['New York', 'London', 'Paris'];
-    return location[Math.floor(Math.random()*3)]
+    location = ['New York', 'London', 'Paris', 'Dublin'];
+    return location[Math.floor(Math.random()*4)]
 };
 
 function instrument(){
-    instrument = ['Piano', 'Double Bass', 'Flute'];
-    return instrument[Math.floor(Math.random()*3)]
+    instrument = ['Piano', 'Double Bass', 'Flute', 'Drums'];
+    return instrument[Math.floor(Math.random()*4)]
 };
 
 function famousPerson(){
-    famousPerson = ['Elton John', 'Boris Johnson', 'Drake'];
-    return famousPerson[Math.floor(Math.random()*3)]
+    famousPerson = ['Elton John', 'Boris Johnson', 'Drake', 'Rob Eskins'];
+    return famousPerson[Math.floor(Math.random()*4)]
 };
 
 console.log(createMessage(location(),instrument(),famousPerson()))
